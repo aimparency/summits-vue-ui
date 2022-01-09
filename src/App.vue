aab51513d6bff950dc4356051ee0837aa9d72691
Felix Niemeyer <niemeyer.felix@gmail.com>
2022-01-09 17:38:14 +0300

    svg with vue test successful. let's go

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <SVGTest msg="Welcome to Your Vue.js + TypeScript App"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import SVGTest from './components/SVGTest.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld, 
+    SVGTest
+  }
+});
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SVGTest.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="svgtest">
+    <p> here is a svg element, let's bring this to work in vue </p>
+    <svg>
+      <SVGPath/>
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SVGPath from './SVGPath.vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  components: {
+    SVGPath
+  },
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
